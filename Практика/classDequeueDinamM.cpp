e1bb2fb404a0ae361a397dfe9ebfe39934bfae10
Карина Тушенцова <karinka25032003@gmail.com>
2022-11-05 15:49:36 +0300

    Update classDequeueDinamM.cpp

--- Практика/classDequeueDinamM.cpp
+++ Практика/classDequeueDinamM.cpp
@@ -3,104 +3,169 @@
 #define N 5
 using namespace std;
 
-class Dequeue
-{
-    int buffer[N];
-    int start = -1, end = 0;
-
+class Dequeue {
+    int start = -1;
+    int end = -1;
+    int size = N;
+    int * buffer = new int[size];
+    
+    bool IsFull() const {
+        return  (end + 1)%size == start ;
+    } 
+    bool OneElement() const{
+        return (start == end && start != -1);
+    }
+    
 public:
-    Dequeue() = default;
-
-    bool IsEmpty() const
-    {
-        return (start == -1);
+    Dequeue()= default;
+    ~Dequeue() {
+        delete [] buffer;
     }
 
-    bool IsFull() const
-    {
-        return ((end + 1) % N == start);
+    Dequeue(const Dequeue & obj) {
+        size = obj.size;
+        int i = obj.start, j = 0;
+        while (i != obj.end) {
+            buffer[j] = obj.buffer[i];
+            i = (i + 1) % size;
+            j++;
+        }
+        start = 0;
+        end = j;
+        buffer[end] = obj.buffer[i];
     }
-
-    bool InsertStart(int element)
-    {
-        if (IsFull())
+    
+    bool IsEmpty() const {
+        return (start == end && start == -1);
+    }
+    int get_count() const {
+        return (end >= start) ? (end-start+1) : size - (start - end - 1);                                               
+    }
+    int get_front() const {
+        if(IsEmpty()) 
+            exit(1);
+        return buffer[start];
+    }
+    int get_back() const {
+        if(IsEmpty())
+            exit(1);
+        return buffer[end];
+    }
+    
+    void print() const {
+        if(IsEmpty()) return ;
+        int i = start;
+        while(i != end) {
+            cout << buffer[i] << " -> ";
+            i = (i + 1)%size;
+        }
+        cout << buffer[i] << endl;       
+    }
+    
+    bool pop_front() {
+        if(IsEmpty()) 
             return false;
-        if (IsEmpty())
+        if(OneElement()) 
+            start = end = -1;
+        else 
+            start = (start + 1) % size;
+        return true;
+    }
+    
+    bool pop_back() {
+        if(IsEmpty()) 
+            return false;
+        if(OneElement()) 
+            start = end = -1;
+        else 
+            end = (end - 1 + size) % size;
+        return true;
+    }
+
+    bool push_front(int element) {
+        if(IsEmpty())  
             start = end = 0;
-        else if (start == 0)
-            start = N - 1;
-        else
-            start--;
+        else {
+            if(IsFull()) {
+                int * new_buffer = new int[size*2];
+                int j = start, i = 0;
+                while(j != end) {
+                    new_buffer[i] = buffer[j];
+                    i++;
+                    j = (j + 1)%size;
+                }
+                new_buffer[i] = buffer[j];
+                delete [] buffer;
+                buffer = new_buffer;
+                start = 0;
+                end = i;
+                size *= 2;
+            }
+            start = (start - 1 + size)%size;
+        }
         buffer[start] = element;
         return true;
     }
 
-    bool InsertEnd(int element)
-    {
-        if (IsFull())
-            return false;
-        if (IsEmpty())
+    bool push_back(int element) {
+        if(IsEmpty())  
             start = end = 0;
-        else
-            end = (end + 1) % N;
+        else {
+            if(IsFull()) {
+                int * new_buffer = new int[size*2];
+                int j = start, i = 0;
+                while(j != end) {
+                    new_buffer[i] = buffer[j];
+                    i++;
+                    j = (j + 1) % size;
+                }
+                new_buffer[i] = buffer[j];
+                delete [] buffer;
+                buffer = new_buffer;
+                start = 0;
+                end = i;
+                size *= 2;
+            }
+            end = (end + 1)%size;
+        }
         buffer[end] = element;
         return true;
     }
 
-    bool DeleteStart()
-    {
-        if (IsEmpty())
-            return false;
-        if (start == end)
-            start = end = -1;
-        else
-            start = (start + 1) % N;
-        return true;
-    }
-
-    bool DeleteEnd()
-    {
-        if (IsEmpty())
-            return false;
-        if (start == end)
-            start = end = -1;
-        else if (end == 0)
-            end = N - 1;
-        else
-            end--;
-        return true;
-    }
-
-    void print() const
-    {
-        if (IsEmpty())
-            cout << "The qeueu is empty \n";
-        else if (end == start)
-            cout << buffer[start] << endl;
-        else
-        {
-            cout << "start = ";
-            int i = start;
-            while (i != end)
-            {
-                cout << buffer[i] << " -> ";
-                i = (i + 1) % N;
+    const Dequeue operator=(const Dequeue &obj) {
+        if (&buffer != &obj.buffer) {
+            size = obj.size;
+            int i = obj.start, j = 0;
+            while (i != obj.end) {
+                buffer[j] = obj.buffer[i];
+                i = (i + 1) % size;
+                j++;
             }
-            cout << buffer[i] << endl;
+            start = 0;
+            end = j;
+            buffer[end] = obj.buffer[i];
         }
+        return *this;
     }
 };
 
-int main()
-{
-    Dequeue d1;
-    d1.InsertStart(1);
-    d1.InsertStart(2);
-    d1.InsertStart(3);
-    d1.DeleteStart();
-    d1.InsertEnd(4);
-    d1.DeleteEnd();
+int main() {
+    Dequeue q1;
+    q1.push_front(1);
+    q1.push_back(2);
+    q1.push_front(3);
+    q1.push_front(4);
+    q1.push_back(5);
+    q1.pop_back();
+    q1.pop_front();
+    
+    Dequeue q2(q1);
+    q1.print();
+    q2.print();
 
-    d1.print();
-    return 0;
+    q2 = q1;
+    q1.print();
+    q2.print();
+
+    return 0;  
 }
